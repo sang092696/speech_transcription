9421ea738d20b0b88424c5bc444cb766fec3299a
Sanghyun Lee <sang0926@gmail.com>
2024-06-20 15:06:27 -0400

    added functionality of speech to text

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from flask import Flask, render_template
+import io
+import os
+from flask import Flask, render_template,request
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv() #loads all env vars defined in '.env' into 'os.environ'
+client= OpenAI(api_key = os.environ['OPENAI_API_KEY'])
 
 def create_app():
     app = Flask(__name__)
@@ -6,5 +13,20 @@ def create_app():
     @app.route("/")
     def index():
         return render_template("index.html")
+    
+    @app.route("/transcribe", methods=["POST"])
+    def transcribe():
+        file =request.files["audio"]
+
+        #convert file into a format that OpenAI can read
+        buffer = io.BytesIO(file.read())
+        buffer.name = "audio.webm"
+
+        transcript = client.audio.transcriptions.create(
+            model="whisper-1",
+            file=buffer
+        )
+
+        return {"output":transcript.text}
     
     return app
--- static/app.js
+++ static/app.js
@@ -1,2 +1,55 @@
 const record = document.querySelector(".record");
 const output = document.querySelector(".output");
+
+//check for available web api 
+if (navigator.mediaDevices.getUserMedia){
+
+    let onMediaSetupSuccess = function (stream) {
+        const mediaRecorder = new MediaRecorder(stream);
+        //
+        let chunks = [];
+
+        record.onclick = function() {
+            if (mediaRecorder.state == "recording"){
+                mediaRecorder.stop();
+                //bootstrap stuff
+                record.classList.remove("btn-danger");
+                record.classList.add("btn-primary");
+            } else{
+                mediaRecorder.start();
+                record.classList.remove("btn-primary");
+                record.classList.add("btn-danger");
+            }
+        }
+
+        mediaRecorder.ondataavailable = function (e) {
+            chunks.push(e.data);
+        }
+
+        mediaRecorder.onstop = function () {
+            let blob = new Blob(chunks, {type: "audio/webm"});
+            chunks = [];
+
+            //send data to flask app to interact with API
+            let formData = new FormData();
+            formData.append("audio",blob);
+
+            fetch("/transcribe",{
+                method:"POST",
+                body:formData
+            }).then((response) =>response.json())
+            .then((data) =>{
+                output.innerHTML = data.output;
+            })
+        }
+    }
+
+    let onMediaSetupFailure = function (err) {
+        alert(err);
+    }
+
+    navigator.mediaDevices.getUserMedia({audio: true}).then(onMediaSetupSuccess,onMediaSetupFailure);
+
+}else{
+    alert("getUserMedia not supported in this browser")
+}
